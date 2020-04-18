20d18d25925a00c9eeab7c953c0fd1d96f477410
Fractured2K <32372095+Fractured2K@users.noreply.github.com>
2020-04-17 20:26:17 -0700

    MVP -> TESTS PASSED: 998 moves, 500 rooms visited

--- adv.py
+++ adv.py
@@ -28,8 +28,65 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# Graph of traversed rooms (it's really just a dictionary shh)
+graph = {}
+# Path for backtracking
+current_path = []
+# Dictionary containing opposite directions (useful for backtracking)
+opposite_direction = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
 
+def next_direction(current_room):
+    """
+    Helper function that returns a direction to move in
+    or None if you're currently at a dead end
+    """
+    for direction, room in current_room.items():
+        if room is '?':
+            return direction
+    return None
 
+while len(graph) != len(world.rooms):
+    # Reference to current room
+    current_room = player.current_room
+    current_room_id = player.current_room.id
+    previous_room_id = None
+
+    # Check if the current room is stored in the graph
+    if current_room_id not in graph:
+        # Get every possible direction to move in the current room 
+        graph[current_room_id] = { direction: '?' for direction in player.current_room.get_exits()}
+    
+    # Choose direction to go in
+    direction = next_direction(graph[current_room_id])
+    
+    # If dead end
+    if direction is None:
+        new_path = current_path.pop()
+        player.travel(opposite_direction[new_path])
+        traversal_path.append(opposite_direction[new_path])
+        continue
+
+    # Update reference to previous rooom
+    previous_room_id = player.current_room.id
+    # Travel to generated directioon
+    player.travel(direction)
+    # Add direction traveled to traversal path
+    traversal_path.append(direction)
+    # Add direction traveled to current_path (for backtracking)
+    current_path.append(direction)
+    # Update reference of current room
+    current_room = player.current_room
+    current_room_id = player.current_room.id
+
+    # Check if the current room is stored in the graph
+    if current_room_id not in graph:
+        # Get every possible direction to move in the current room 
+        graph[current_room_id] = { direction: '?' for direction in player.current_room.get_exits()}
+
+    # Link rooms together
+    if graph[current_room_id][opposite_direction[direction]] == '?':
+        graph[current_room_id][opposite_direction[direction]] = previous_room_id
+        graph[previous_room_id][direction] = current_room_id
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -51,12 +108,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
